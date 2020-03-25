a79153e9c3fc47aac6d1b7e8b634829f20147e4e
Takumasa Hirabayashi <baa_ram_ewe_shipping@live.jp>
2020-03-25 23:55:52 +0900

    add a constructor to validate length

--- line-messaging-api-csharp/Messages/Sender.cs
+++ line-messaging-api-csharp/Messages/Sender.cs
@@ -1,4 +1,6 @@
-﻿namespace LineDC.Messaging.Messages
+﻿using System;
+
+namespace LineDC.Messaging.Messages
 {
     /// <summary>
     /// When sending a message from the LINE Official Account, you can specify the sender in Message objects.
@@ -8,12 +10,28 @@
         /// <summary>
         /// Display name. Certain words such as LINE may not be used. (Max: 20 characters)
         /// </summary>
-        public string Name { get; set; }
+        public string Name { get; }
 
         /// <summary>
         /// URL of the image to display as an icon when sending a message. (Max: 1000 characters)
         /// HTTPS
         /// </summary>
-        public string IconUrl { get; set; }
+        public string IconUrl { get; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name">
+        /// Display name. Certain words such as LINE may not be used. (Max: 20 characters)
+        /// </param>
+        /// <param name="iconUrl">
+        /// URL of the image to display as an icon when sending a message. (Max: 1000 characters)
+        /// HTTPS
+        /// </param>
+        public Sender(string name, string iconUrl)
+        {
+            Name = name.Substring(0, Math.Min(name.Length, 20));
+            IconUrl = iconUrl;
+        }
     }
 }
